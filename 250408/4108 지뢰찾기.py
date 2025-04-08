33330058bd954c71ec75035f4b2e4f101a48f9a6
jinmo <lsoul1027@gmail.com>
2025-04-08 17:59:20 +0900

    r

---
+++ 250408/4108 지뢰찾기.py
@@ -0,0 +1,14 @@
+# 비어있는 칸 마다 주변에 지뢰가 몇개 있는지
+# 
+
+while True:
+    R,C = map(int,input().split())
+    if R == 0 and C == 0:
+        break
+    Bomb = [list(input()) for _ in range(R)]
+
+    
+
+
+
+
