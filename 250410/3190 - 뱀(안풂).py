140ee3519e0a85164d8e90822e6fa2bc45829a70
jinmo <lsoul1027@gmail.com>
2025-04-11 00:37:03 +0900

    3190 - 뱀(순서 문제)

--- 250410/3190 - 뱀(안풂).py
+++ 250410/3190 - 뱀(안풂).py
@@ -1,46 +1,56 @@
 from collections import deque
-def snake_road(s):
+def snake_road():
     visited = [[0]* N for _ in range(N)]
     q = deque()
-    q.append((0,0,1)) # r, c, time
-    visited[0][0] = 9 # 뱀 몸이 있는 곳은 9로 표시 
+    q.append((0,0)) # r, c, time
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
 
-    di = []
-    dj = []
-    while q:
-        i,j,time = q.popleft()
-        if i < 0 or j <0 or i >= N or j >= N or visited[i][j] == 9:
+    turn_idx = time = 0
+    i = j = 0
+    while True:
+        # 종료 조건
+        if i < 0 or j < 0 or i >= N or j >= N or board[i][j] == 9:
             return time
-        # 방향 바꿔줄 시간일 때
-        if time == int(x):
-            if c == 'L':
-                if (i,j) == (-1,0):
 
-                elif
-                for di,dj in ()
-            elif c == 'D':
-                pass
-        # 일반적일 때
-        else:
+        # 사과 체크 후 꼬리 변경
+        if time >= 1:
+            if board[i][j] == 0:
+                a, b = q.popleft()
+                board[a][b] = 0
 
+        # 이동하기 전 시간 먼저 체크
+        if time in turn_time:
+            if turn_time[time] == 'L':  # 왼쪽이면
+                turn_idx -= 1
+                # if abs(turn_idx) <= -4:
+                #     turn_idx = turn_idx % 4
 
+            elif turn_time[time] == 'D':  # 오른쪽이면
+                turn_idx += 1
+                # if abs(turn_idx) >= 4:
+                #     turn_idx = turn_idx % 4
+            turn_idx = abs(turn_idx % 4)
+
+        # 방문 체크
+        q.append((i,j))
+        board[i][j] = 9
+        i, j = i + di[turn_idx], j + dj[turn_idx]  # 다음 갈 위치
+        time += 1
 
 
 N = int(input()) # 보드 길이
 K = int(input()) # 사과의 개수 
 board = [[0]*N for _ in range(N)]
 for i in range(K):
-    a, b = map(int,input())
+    a, b = map(int,input().split())
     board[a-1][b-1] = 1 # 사과 체크
-L = int(input()) # 뱀 방향 변환 횟수
 
+L = int(input()) # 뱀 방향 변환 횟수
+turn_time = {} # 방향전환 값 받아줄 딕셔너리
 for i in range(L):
     # x초가 끝난 뒤에 왼쪽(L) 또는 오른쪽(D)으로 90도 방향 회전
     x, c = map(str,input().split())
-    
+    turn_time[int(x)] = c # 딕셔너리 추가
 
-## 어떻게 방향바꿀때 확인하지?
-## 왼쪽 오른쪽 정해졌고 바꿀때 델타를 어떻게 정해주지 ?
-
-
-
+print(snake_road())
