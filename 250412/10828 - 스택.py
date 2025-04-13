c9af067bfbd3f1349672478cea2a0cf373fac11f
jinmo <lsoul1027@gmail.com>
2025-04-13 14:27:28 +0900

    스택

---
+++ 250412/10828 - 스택.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+
+N =int(sys.stdin.readline())
+
+for i in range(N):
+    input_value = sys.stdin.realine().split()
+    command = input_value[0]
+
+    if command == 'push':
+        pass
+
+    elif command == 'pop':
+        pass
+
+    elif command == 'size':
+        pass
+
+    elif command == 'empty':
+        pass
+
+    elif command == 'top':
+        pass
+    
