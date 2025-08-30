e1587f0ba29ce50dba5e263161bc91a3d8c453ff
이진모 <lsoul1027@gmail.com>
2025-08-30 23:38:59 +0900

    1075 - 나누기

---
+++ 2025/month/8/bronze/1075 - 나누기.py
@@ -0,0 +1,9 @@
+N,F = map(int,input().split())
+N = int(input())
+F = int(input())
+
+base = (N // 100) * 100
+for i in range(100):
+    if (base + i) % F == 0:
+        print(f"{i:02d}")
+        break
