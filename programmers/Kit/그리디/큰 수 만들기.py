152dfa6aa5f4115b9b2d444c0bbff2a8f3486f69
jinmo <lsoul1027@gmail.com>
2026-02-03 15:08:11 +0900

    큰 수 만들기

---
+++ programmers/Kit/그리디/큰 수 만들기.py
@@ -0,0 +1,11 @@
+def solution(number, k):
+    stack = []
+    
+    for num in number:   
+        while stack and k > 0 and stack[-1] < num:
+            stack.pop()
+            k -= 1
+
+        stack.append(num)
+    answer = ''.join(stack[:len(stack)-k])
+    return str(answer)
